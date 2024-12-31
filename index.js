827e527559ce2f1707560f2fb256be447e208ba8
sunny prajapati <sunnyprajapati9761@gmail.com>
2024-12-31 11:29:00 +0530

    finaldbcommit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require("mongoose");
 const app = express();
 app.use(express.json())
-const router = require("./routes/studentRoute")
+
 const bookrouter = require("./routes/bookRoute");
 const dotenv = require('dotenv')
 dotenv.config();    
